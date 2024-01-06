b8707c0738a2aa38188b777318c522eac20be0a3
cristian-albu <albucristian95p@gmail.com>
2024-01-06 19:59:01 +0200

    comment out func

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,18 @@ import HomeView, { THomeViewData } from "@/views/home-view/HomeView";
 import { Entry } from "contentful";
 import React from "react";
 
-const getPosts = async (): Promise<Entry<any>[]> => {
-    try {
-        const res = await client.getEntries({
-            content_type: "blogPost",
-        });
+// const getPosts = async (): Promise<Entry<any>[]> => {
+//     try {
+//         const res = await client.getEntries({
+//             content_type: "blogPost",
+//         });
 
-        return res.items;
-    } catch (error) {
-        console.log(error);
-        throw new Error("Something went wrong");
-    }
-};
+//         return res.items;
+//     } catch (error) {
+//         console.log(error);
+//         throw new Error("Something went wrong");
+//     }
+// };
 
 const HomePage = async () => {
     const data: THomeViewData = {
